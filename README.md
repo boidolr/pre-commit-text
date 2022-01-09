3f46bc15c50424f978bd6ecc5e621509a4278548
Raphael Boidol <boidolr@users.noreply.github.com>
2022-01-09 12:36:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+pre-commit-text [![tag](https://img.shields.io/github/v/tag/boidolr/pre-commit-text?sort=semver)](https://github.com/boidolr/pre-commit-text/tags) [![Build](https://github.com/boidolr/pre-commit-text/actions/workflows/continous-integration.yml/badge.svg)](https://github.com/boidolr/pre-commit-text/actions/workflows/continous-integration.yml) [![pre-commit](https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit&logoColor=white)](https://github.com/pre-commit/pre-commit)
+================
+
+A collection of git hooks for use with the [pre-commit](https://github.com/pre-commit/pre-commit) framework. For details see the list of available hooks below.
+
+## Using pre-commit-text with pre-commit
+
+Add this to your `.pre-commit-config.yaml`:
+```
+    -   repo: https://github.com/boidolr/pre-commit-text
+        rev: v1.0.0  # Use the ref you want to point at
+        hooks:
+        -   id: replace-tabs
+        # -   id: ...
+```
+For an extended example see [`.pre-commit-config.yaml`](.pre-commit-config.yaml).
+
+## Available hooks
+
+- **`properties-whitespace`**: Remove whitespace around equal signs in property files.
+  Implemented using the `search-replace` hook.
+- **`replace-tabs`**: Replace tabs in files.
+    - `--tabsize` spaces to replace a tab with.
+- **`search-replace`**: Replace patterns in files.
+    - `--search` regular expression to use for search.
+    - `--replacement` replacement for matches.
---
+++ hooks/search_replace.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import argparse
+import re
+import sys
+from typing import Optional
+from typing import Pattern
+from typing import Sequence
+
+
+def _search_replace(filename: str, pattern: Pattern[bytes], replacement: bytes) -> int:
+    with open(filename, "rb") as fh:
+        content = fh.readlines()
+
+    ret = 0
+    for index, line in enumerate(content):
+        processed = pattern.sub(replacement, line)
+        if processed != line:
+            print(f"{filename}:{index + 1}:", end="")
+            print(line.rstrip(b"\r\n").decode(errors="replace"))
+            content[index] = processed
+            ret = 1
+
+    if ret != 0:
+        with open(filename, "wb") as fh:
+            fh.writelines(content)
+
+    return ret
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", nargs="*", help="File names to search")
+    parser.add_argument(
+        "-s", "--search", dest="pattern", help="Regular expression to use for search"
+    )
+    parser.add_argument(
+        "-r", "--replacement", help="Replacement for successful matches"
+    )
+    args = parser.parse_args(argv)
+
+    ret = 0
+    pattern = re.compile(args.pattern.encode())
+    replacement = args.replacement.encode()
+    for filename in args.filenames:
+        ret |= _search_replace(filename, pattern, replacement)
+    return ret
+
+
+if __name__ == "__main__":
+    sys.exit(main())
